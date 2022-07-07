684ec654f551524d14005b963b905668f54862c3
celticcow <gregory.dunlap@gmail.com>
2022-07-07 14:40:46 -0500

    can divide

--- tester.py
+++ tester.py
@@ -21,6 +21,25 @@ def ask_multiply():
         return(0)
 #end of ask_multipy
 
+def ask_divide():
+    num1 = int(random.randint(1, 10))
+    num2 = int(random.randint(1, 10))
+
+    product = num1 * num2
+
+    print("please Divide : ", end="")
+    print(str(product) + " / " + str(num1))
+
+    user_answer = int(input())
+
+    if(user_answer == num2):
+        print("CORRECT")
+        return(1)
+    else:
+        print("sorry, the correct answer is : " + str(num2))
+        return(0)
+#end ask_divide
+
 def main():
     print("welcome to maths")
 
@@ -32,10 +51,13 @@ def main():
 
     grade = 0
     for i in range(10):
-        #print(i)
-        grade += ask_multiply()
+        print(i)
+        #grade += ask_multiply()
+        #grade += ask_divide()
 
     print("you got " + str(grade) + " out of 10")
+
+    
 #end of main
 
 if __name__ == "__main__":
